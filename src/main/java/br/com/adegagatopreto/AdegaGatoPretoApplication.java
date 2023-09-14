b0b660f09f17367591a2cc0aeb2e97b0d0080c10
Danilo Angeli <dani.angeli97@gmail.com>
2023-09-14 16:01:30 -0300

    SpringBoot Project Created

---
+++ src/main/java/br/com/adegagatopreto/AdegaGatoPretoApplication.java
@@ -0,0 +1,13 @@
+package br.com.adegagatopreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdegaGatoPretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdegaGatoPretoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adegagatopreto/AdegaGatoPretoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adegagatopreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdegaGatoPretoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
